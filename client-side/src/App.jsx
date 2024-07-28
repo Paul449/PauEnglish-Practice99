b2f892872963d415bf26648e1c3d740494c26fbe
paul449 <paul_bilbatua@hotmail.com>
2024-07-27 19:33:55 -0500

    landing

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+import './index.css'
+import landing from'./pages/landing'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+     <landing/>
     </>
   )
 }
---
+++ client-side/src/pages/landing.jsx
@@ -0,0 +1,19 @@
+
+
+function landing(){
+
+    return(
+        <>
+    <main>
+      <h1>PauEnglish</h1>
+      <p>
+        Welcome to PauEnglish, the best app where you can develop<br/>
+        your English skills by discussing specific topics with an AI bot.
+      </p>
+      <p>Start Today</p>
+      <button>Speak with chatbot</button>
+    </main>
+        </>
+    )
+}
+export default landing;
