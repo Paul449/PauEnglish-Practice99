33245ee713332dcd109e0057748b539d5deed860
paul449 <paul_bilbatua@hotmail.com>
2024-11-20 16:20:25 -0600

    fix

--- client-side/src/pages/Chat.jsx
+++ client-side/src/pages/Chat.jsx
@@ -1,14 +1,15 @@
 /*import react bootstrap components needed*/
-import button from "react-bootstrap/button";
 export default function Chat(){
     return(
     <>
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-md-4 col-md-2">
-            
+            Column 1
           </div>
-          <div className="col-sm-6 col-md-4 col-md-2">Col Two</div>
+          <div className="col-sm-6 col-md-4 col-md-2">
+            Column 2
+          </div>
         </div>
       </div>  
     </>
