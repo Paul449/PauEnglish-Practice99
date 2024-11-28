c04facac7c30076d5bbf68098e30986e163758c3
paul449 <paul_bilbatua@hotmail.com>
2024-11-28 13:20:59 -0600

    dependencies

--- client-side/src/pages/Chat.jsx
+++ client-side/src/pages/Chat.jsx
@@ -2,23 +2,27 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import OnlineBar from '../components/onlineBar';
+import SearchBox from '../components/searchBox';
+//import OnlineBar from '../components/onlineBar';
 export default function Chat(){
     return(
     <>
-    <Container fluid = 'lg' className='vh-100' >
-     <Row className='h-100' lg={4} >
-      <Col lg = {3} className='conversations h-100'>
+    <Container fluid ='lg' className='vh-100' >
+     <Row className='no-gutters' >
+      <Col lg = {4} className='conversations h-100'>
+      <div className='settings-tray'>
+      <div className='profile-image'><img src=''alt='myself'/></div>  
       <h5>Saved Conversations:</h5>
+      </div>
+      <div className='Search-bar'>
+        <SearchBox/>
+      </div>
       <div className='conver-list'>
 
       </div>
       </Col>
-      <Col lg={9}>
-      <OnlineBar/>
-      </Col>
-      <Col lg = {9} className=' chatLayout h-100'>
-      
+      <Col lg={8} className=' chatLayout h-100'>
+    
       </Col>
      </Row>
     </Container>
