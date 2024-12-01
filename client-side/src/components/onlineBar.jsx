1201a59e41db3e0ceb9419ecdee9b9714cdca93a
paul449 <paul_bilbatua@hotmail.com>
2024-12-01 17:34:09 -0600

    online

--- client-side/src/components/onlineBar.jsx
+++ client-side/src/components/onlineBar.jsx
@@ -4,9 +4,12 @@ function OnlineBar(){
      <div className="Online">
         <div className="chatBot">
         <img id="Bot" src="https://www.shutterstock.com/image-vector/happy-robot-3d-ai-character-600nw-2464455965.jpg" height={'55px'} width={'55px'}/>
+        <div className="chatOnline">
         <p>ChatBot</p>
+        <p>Online</p>
         </div>
-        <p id="IF">Online</p>
+        </div>
+        <div className="topicTimer"></div>
     </div>
     </>)
 }
