bf63904dfabd8e7168dedd9b4a455e6c698b1bfd
paul449 <paul_bilbatua@hotmail.com>
2024-12-01 21:57:05 -0600

    last

--- client-side/src/components/searchBox.jsx
+++ client-side/src/components/searchBox.jsx
@@ -1,15 +1,11 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 function SearchBox(){
 return(<>
-    <div className="input-group">
-  <div className="form-outline" data-mdb-input-init>
-    <input type="search" id="form1" className="form-control" />
-    <label className="form-label" htmlFor="form1">
-      Search
-    </label>
-  </div>
-  <button type="button" className="btn btn-primary" data-mdb-ripple-init>
-    <i className="fas fa-search"></i>
-  </button>
+<div className="input-group">
+  <input placeholder="Search...">
+  </input>
+  <button><i className="fas fa-search"></i></button>
 </div>
 
     </>)
