12355220a6dc8facc6962064611555921ed2a5fd
paul449 <paul_bilbatua@hotmail.com>
2025-01-26 20:56:47 -0600

    test basic server

--- server-side/index.js
+++ server-side/index.js
@@ -2,3 +2,15 @@
 let HTTP = require('http');
 //import api key
 let secretKey = process.env.OPENAI_KEY;
+//import express
+let express = require('express');
+//create app
+let app = express();
+//define port number
+let PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.get('/',(req,res)=>{res.send('hello')});
+//test
+app.listen(PORT,()=>{
+ console.log(`listening on http://localhost:${PORT}`)
+})
