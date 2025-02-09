c47b6ff553b4d8c1ff53e74ab74d512f1b303f18
paul449 <paul_bilbatua@hotmail.com>
2025-02-09 13:57:48 -0600

    more scehmas

--- server-side/models/converschema.js
+++ server-side/models/converschema.js
@@ -1,7 +1,7 @@
 import mongoose from "../config/connection";
 
 const converschema = new mongoose.Schema({
-    converID: {type:Number, required:true},
+    converID: {type:Number, required:true, unique:true},
 })
 const Conversation = mongoose.model('conversation', converschema);
 module.exports = Conversation;
