9cc874014681f82405af349c37e511b785e8d8bc
paul449 <paul_bilbatua@hotmail.com>
2025-02-09 13:22:11 -0600

    topic schema

--- server-side/models/Topic.js
+++ server-side/models/Topic.js
@@ -1,1 +1,10 @@
-import mongoose from "../config/connection";
+import mongoose from "../config/connection"; //connecting to PauEnglish DB
+//import model from mongoose library to create my topic database and store each one
+import { model,Schema } from "mongoose";
+//defining topic schema to structue collection of documents
+const TopicSchema = new Schema({
+    id:{type: Number, required: true, unique: true},
+    name: {type: String, required:true, unique: true},
+});
+const Topic = model('Topic',TopicSchema);
+module.exports = Topic;
