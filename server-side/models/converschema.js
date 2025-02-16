58aac2e0ee731ff4797c6dad5535e85affa9677c
paul449 <paul_bilbatua@hotmail.com>
2025-02-16 15:47:51 -0600

    converID

--- server-side/models/converschema.js
+++ server-side/models/converschema.js
@@ -1,7 +1,27 @@
 import mongoose from "../config/connection";
+import {type} from 'mongoose';
+/*
 
+
+*/
 const converschema = new mongoose.Schema({
-    converID: {type:Number, required:true, unique:true},
+    _id: { //conversaton id saved in whole database
+        type: Number,
+        required: true,
+        unique: true
+        },
+    name: { // name of the topic discussed
+        type: String,
+        required: true
+    },
+    time:{ //timestamp
+        type: Date,
+        default: Date.now
+    },
+    video:{
+
+    }
+
 })
 const Conversation = mongoose.model('conversation', converschema);
 module.exports = Conversation;
