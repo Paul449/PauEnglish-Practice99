75daf2162cb531b2e04d99003d3afd1b53271e7c
paul449 <paul_bilbatua@hotmail.com>
2025-02-16 15:20:35 -0600

    one model

--- server-side/models/Topic.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "../config/connection"; //connecting to PauEnglish DB
-//defining topic schema to structue collection of documents
-const TopicSchema = new mongoose.Schema({
-    id:{type: Number, required: true, unique: true}, // topic id where is stored
-    topic: {type: String, required: true}, // topic name used to interact with openai server through controllers
-});
-const Topic = mongoose.model('Topic',TopicSchema);
-module.exports = Topic;
