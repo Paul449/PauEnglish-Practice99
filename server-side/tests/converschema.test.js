e3327632caa96c47ac1f143f55868e7849c01167
paul449 <paul_bilbatua@hotmail.com>
2025-02-23 18:13:28 -0600

    mongodb-jest

--- server-side/tests/converschema.test.js
+++ server-side/tests/converschema.test.js
@@ -1,7 +1,7 @@
 //import Mongoose model to mock functionality
 import Conversation from '../models/converschema.js';
+const {MongoClient} = require('mongodb');
 //test string
-
 test('topic name',()=>{
     const Conver = Conversation({title:'Music'});// selecting title document from conver schema
     expect(Conver.title).toBe('Music');
