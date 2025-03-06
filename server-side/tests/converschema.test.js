743a27886a105261b1df2b576af5d0842152a969
paul449 <paul_bilbatua@hotmail.com>
2025-03-05 22:52:29 -0600

    test corrections

--- server-side/tests/converschema.test.js
+++ server-side/tests/converschema.test.js
@@ -3,34 +3,38 @@ require('dotenv').config();
 const { connection, default: mongoose } = require('mongoose');
 const {MongoMemoryServer} = require('mongodb-memory-server');
 const {Conversation} = require('../models/converschema.js');
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectId} = require('mongodb');
 //test database suite 
 describe('testing EnglishDB',()=>{
     //define variable to connect with database
     let Connection;
     //define database variable
     let EnglishDB;
-    //connect to database tobefore running all test cases
+    let mongoServer;
+    //connect to database before running all test cases
     beforeAll(async ()=>{
+        mongoServer = await MongoMemoryServer.create();
+        const mongoUri = mongoServer.getUri();
         Connection = await MongoClient.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology:true,
         });
-        EnglishDB = await Connection.db(globalThis.MONGO_DB_NAME);
+        EnglishDB = Connection.db();
     })
     //closes database once all tests ran
     afterAll(async()=>{
         await mongoose.connection.close();
+        await mongoServer.stop();
     })
     it('insert document into the collection',async()=>{
         const Title = EnglishDB.collection('conversation');
         //testing title
         const documentConver = {
-            _id:'ID topic',
+            _id:new ObjectId(),
             title:'Music',
         }
     await Title.insertOne(documentConver);
-    const InsertTopic = await Title.findOne({_id:'ID topic'});
+    const InsertTopic = await Title.findOne({_id:documentConver._id});
     expect(InsertTopic).toEqual(documentConver);
     })
 });
