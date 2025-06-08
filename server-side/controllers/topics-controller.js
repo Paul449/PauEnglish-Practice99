e02759db7f106796e4dd2bb227e12892eb237413
paul449 <paul_bilbatua@hotmail.com>
2025-06-08 14:07:26 -0500

    topic controller

--- server-side/controllers/topics-controller.js
+++ server-side/controllers/topics-controller.js
@@ -1,12 +1,13 @@
 
-// request topic, prepare answer with initial message on the chat
+//request topic
+export default function selectTopic(req,res){
 
+}
+// request topic, prepare answer with initial message on the chat
 export default async function getMessage(req,res,next){
     let message = await req.params({
         user:'chatbot',
         message:req.body,
         timestamp,
     })
-    messsage.json();
-    console.log(message);
 }
