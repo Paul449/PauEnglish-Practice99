ad43b20540e46f3297059a8dc9aa4b672fda0b19
paul449 <paul_bilbatua@hotmail.com>
2025-07-23 16:06:14 -0500

    moving message function to conversation service

--- server-side/services/conversation-service.js
+++ server-side/services/conversation-service.js
@@ -1,5 +1,31 @@
 //importing topic controller
 // response topic, prepare answer with first message on the chat, conducted by model
+async function getMessage(req,res,next){
+    try{
+        let completion = await openai.chat.completions.create({
+         model: "gpt-4",
+         messages:[{
+            role: "user",
+            content:req.body.message || jsonData[key].startedQuestions,
+        }]
+    })
+    const message = completion.choices[0].message.content
+    console.log(completion.model)
+    console.log(message)
+    res.status(200).json({
+        message:message,
+        status:"success"
+    });
+    }catch(error){
+        console.error('something unexpectedly occured',error);
+        //server status
+        res.status(500).json({
+         message:'Failed to get AI response',
+         status:'internal server error',
+         error: error.message
+        })
+    }
+}
 //request to server to talk about selected topic
  const getConversation = async function(){
    const conversation = await getMessage({
